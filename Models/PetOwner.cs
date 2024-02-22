3aaf034ae6def6c8dd5c4e65f790bf47dec3c579
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2024-02-22 10:13:00 -0600

    added PetOwner model

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -3,7 +3,25 @@ using System;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace pet_hotel
+// namesspace for pet owner class
 {
-    public class PetOwner {}
+
+    public class PetOwner
+    // model for pet owners 
+    // id, name, emailAddress, petCount are properties of the class
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string emailAddress { get; set; }
+
+        public int petCount { get; set; }
+
+
+    }
 }
